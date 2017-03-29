d70cef1fb3c09938d8c26d9e2bb54e58dd9e9d7a
Max <neverov.max@gmail.com>
2017-03-29 22:14:38 +0300

    to/from list

--- src/main/kotlin/nmax/either/Either.kt
+++ src/main/kotlin/nmax/either/Either.kt
@@ -14,12 +14,30 @@ class Nil<T> : RList<T>()
 data class Cons<T> constructor(val a: T, val l: RList<T>): RList<T>()
 
 fun main(args: Array<String>) {
-    val list = Cons<Int>(1, Cons<Int>(2, (Cons<Int> (3, Nil()))))
-
-    println(foldl(Int::plus, 0, list))
+    println(foldl(Int::plus, 0, toRList(listOf(1,2,3,4,5))))
 }
 
-fun <A, B> foldl(f: ((B, A) -> B), acc: B, vs: RList<A>): B = when(vs) {
+// foldl :: (a -> b -> a) -> a -> [b] -> a
+fun <A, B> foldl(f: ((A, B) -> A), acc: A, xs: RList<B>): A = when(xs) {
     is Nil -> acc
-    is Cons -> foldl(f, f(acc, vs.a), vs.l)
+    is Cons -> foldl(f, f(acc, xs.a), xs.l)
+}
+
+// fromList :: List a -> [a]
+// fromList Nil = []
+// fromList (Cons x xs) = x : fromList xs
+// cannot use inlined fun with reifined type because of recursion, hence list as a result instead of an array
+fun <T> fromRList(xs: RList<T>) : List<T> = when (xs) {
+    is Nil -> emptyList<T>()
+    is Cons -> fromRList(xs.l).plus(xs.a)
+}
+
+// toList :: [a] -> List a
+// toList [] = Nil
+// toList (x:xs) = Cons x (toList xs)
+fun <T> toRList(l: List<T>) : RList<T> {
+    when {
+        l.isEmpty() -> return Nil<T>()
+        else -> return Cons<T> (l.get(0), toRList(l.subList(1, l.size)))
+    }
 }
